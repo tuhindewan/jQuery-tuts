cdd1f20246f30060c309664042a5d1f6998a848c
tuhindewan <tuhinsshadow@gmail.com>
2017-02-03 19:08:23 -0800

    uses of click events

--- custom.js
+++ custom.js
@@ -1,5 +1,12 @@
-	$ (document) .ready(function(){
-		$ ("p") .click(function(){
-			$ (this) .hide(2000);
-		});
-		});
+$ (document) .ready(function(){
+
+	$ (".hide") .click(function(){
+
+		$ (".box") .hide(4000);
+	});
+
+	$ (".show") .click(function(){
+
+		$ (".box") .show(4000);
+	});
+});
