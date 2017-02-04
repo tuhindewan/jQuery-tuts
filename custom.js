c469c9e9804a173117ddee53010013a0e604957b
tuhindewan <tuhinsshadow@gmail.com>
2017-02-03 23:46:56 -0800

    change body color using change event

--- custom.js
+++ custom.js
@@ -1,18 +1,10 @@
 $ (document) .ready(function(){
+	var color = $ ('.list').val();
+		$ ('body') .css('background',color);
+	$ ('.list') .change(function(){
 
-	$ (':input') .mouseover(function(){
-		$ (':input') .val('Put your mouse back to hide it');
-	});
-
-	$ ('input') .mouseover(function(){
-		$('.box') .show(1000);
-	});
-
-	$ (':input') .mouseout(function(){
-		$ (':input') .val('Hover here to show');
-	});
-	$ ('input') .mouseout(function(){
-		$('.box') .hide(1000);
+		var color = $ ('.list').val();
+		$ ('body') .css('background',color);
 	});
 
 });
